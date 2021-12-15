772cd3a9bfb5777a1e9b991868ca73292d723c3d
christianchiarulli <chrisatmachine@gmail.com>
2021-12-14 19:31:44 -0500

    upadte treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,23 +1,42 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+	return
 end
 
-configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-}
+configs.setup({
+	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	autopairs = {
+		enable = true,
+	},
+	indent = { enable = true, disable = { "yaml", "python" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	autotag = {
+		enable = true,
+		disable = { "xml" },
+	},
+	rainbow = {
+		enable = true,
+		colors = {
+			"Gold",
+			"Orchid",
+			"DodgerBlue",
+			-- "Cornsilk",
+			-- "Salmon",
+			-- "LawnGreen",
+		},
+		disable = { "html" },
+	},
+	playground = {
+		enable = true,
+	},
+})
