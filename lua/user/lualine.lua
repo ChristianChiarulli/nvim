dbbd2c282cbeb9ee4bc5d851d3a8bba96cb3549a
ChristianChiarulli <chris.machine@pm.me>
2021-12-16 16:22:33 -0500

    remove lualine toggleterm

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -79,7 +79,7 @@ lualine.setup({
 		theme = "auto",
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "toggleterm" },
 		always_divide_middle = true,
 	},
 	sections = {
