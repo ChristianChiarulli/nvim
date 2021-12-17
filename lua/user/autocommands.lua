571793265fcd6c5d3c3cb69cd4ee7c70ff31e1c2
ChristianChiarulli <chris.machine@pm.me>
2021-12-16 19:00:05 -0500

    q in spectre panel

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd FileType qf,help,man,lspinfo,spectre_panel nnoremap <silent> <buffer> q :close<CR> 
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
