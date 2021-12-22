4a8fdf13146f13872bb03f8b954d113ebb9533c8
ChristianChiarulli <chris.machine@pm.me>
2021-12-22 05:56:22 -0500

    fix register plugin

--- lua/user/registers.lua
+++ lua/user/registers.lua
@@ -1,6 +1,6 @@
-vim.g.registers_return_symbol = "\n" --'⏎' by default
-vim.g.registers_tab_symbol = "\t" --'·' by default
-vim.g.registers_space_symbol = " " --' ' by default
+-- vim.g.registers_return_symbol = "\n" --'⏎' by default
+-- vim.g.registers_tab_symbol = "\t" --'·' by default
+-- vim.g.registers_space_symbol = " " --' ' by default
 vim.g.registers_delay = 0 --0 by default, milliseconds to wait before opening the popup window
 vim.g.registers_register_key_sleep = 0 --0 by default, seconds to wait before closing the window when a register key is pressed
 vim.g.registers_show_empty_registers = 1 --1 by default, an additional line with the registers without content
