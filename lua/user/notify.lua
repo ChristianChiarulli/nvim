a3f6143c8cab49d5f8b23fe045dd1ecb8bfbdfd1
ChristianChiarulli <chris.machine@pm.me>
2021-12-23 11:54:22 -0500

    increase notification timeout

--- lua/user/notify.lua
+++ lua/user/notify.lua
@@ -17,7 +17,7 @@ notify.setup {
   render = "default",
 
   -- Default timeout for notifications
-  timeout = 100,
+  timeout = 175,
 
   -- For stages that change opacity this is treated as the highlight behind the window
   -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
