be08f9cddb4942d44953f6ff2b4851f21ec85656
ChristianChiarulli <chris.machine@pm.me>
2021-12-28 02:45:57 -0500

    updates

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -15,7 +15,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("p", "  Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
 	dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("s", "  Find Session", ":Telescope sessions save_current=false <CR>"),
