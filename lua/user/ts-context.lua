b6caab65b53e3ca40087b468d058003ee7ee0295
ChristianChiarulli <chris.machine@pm.me>
2022-01-06 22:40:44 -0500

    pcall for context

--- lua/user/ts-context.lua
+++ lua/user/ts-context.lua
@@ -1,4 +1,9 @@
-require'treesitter-context'.setup{
+local status_ok, context = pcall(require, "treesitter-context")
+if not status_ok then
+	return
+end
+
+context.setup{
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
     throttle = true, -- Throttles plugin updates (may improve performance)
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
