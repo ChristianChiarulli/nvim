ccd96ced68d07303b9cbea2ccea319761d782de0
ChristianChiarulli <chris.machine@pm.me>
2022-01-10 11:42:12 -0500

    remvoe confirm_done

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -25,9 +25,9 @@ npairs.setup {
   },
 }
 
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-local cmp_status_ok, cmp = pcall(require, "cmp")
-if not cmp_status_ok then
-  return
-end
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+-- local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+-- local cmp_status_ok, cmp = pcall(require, "cmp")
+-- if not cmp_status_ok then
+--   return
+-- end
+-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
