d7d3e9d7a6cda7b30c64176933d611fde0925662
ChristianChiarulli <chris.machine@pm.me>
2022-01-31 11:33:06 -0500

    use colorcolumn

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -44,7 +44,7 @@ vim.g.indent_blankline_context_patterns = {
   "operation_type",
 }
 -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
-vim.wo.colorcolumn = "99999"
+-- vim.wo.colorcolumn = "99999"
 
 -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
 -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
