4dcf918c23772954a909b6885dbfd87deda0f190
ChristianChiarulli <chris.machine@pm.me>
2022-02-05 23:47:20 -0500

    disabel context for now

--- lua/user/ts-context.lua
+++ lua/user/ts-context.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 context.setup{
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
     throttle = true, -- Throttles plugin updates (may improve performance)
     max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
     patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
