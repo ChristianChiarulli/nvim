8959412c64772d5646c848cfe7097acba03baa01
ChristianChiarulli <chris.machine@pm.me>
2022-02-06 02:59:28 -0500

    add function

--- lua/user/functions.lua
+++ lua/user/functions.lua
@@ -51,4 +51,10 @@ end
 
 vim.cmd [[ command! SnipRunToggle execute 'lua require("user.functions").toggle_sniprun()' ]]
 
+-- get length of current word
+function M.get_word_length()
+  local word = vim.fn.expand("<cword>")
+  return #word
+end
+
 return M
