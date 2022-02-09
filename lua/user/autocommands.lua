cb882192810483b74e999b52e119a57a01e21456
ChristianChiarulli <chris.machine@pm.me>
2022-02-09 01:22:18 -0500

    updates

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -5,6 +5,7 @@ vim.cmd [[
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
+    autocmd CmdWinEnter * quit
   augroup end
 
   augroup _git
