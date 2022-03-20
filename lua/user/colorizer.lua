09997e6e31af1f93d42083f56380c9679d5c93d0
ChristianChiarulli <chris.machine@pm.me>
2022-03-20 16:15:18 -0400

    don't color names

--- lua/user/colorizer.lua
+++ lua/user/colorizer.lua
@@ -10,8 +10,8 @@ colorizer.setup({ "*" }, {
   RRGGBBAA = true, -- #RRGGBBAA hex codes
   rgb_fn = true, -- CSS rgb() and rgba() functions
   hsl_fn = true, -- CSS hsl() and hsla() functions
-  css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-  css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
   -- Available modes: foreground, background, virtualtext
   mode = "background", -- Set the display mode.)
 })
