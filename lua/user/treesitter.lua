1d418c0c87e9dc82856ca4571d6c23caba84ba77
ChristianChiarulli <chris.machine@pm.me>
2022-03-20 03:46:45 -0400

    updates for indents

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -17,7 +17,7 @@ configs.setup({
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { "yaml", "python", "css" } },
+	indent = { enable = true, disable = { "python", "css" } },
 	context_commentstring = {
 		enable = true,
 		enable_autocmd = false,
