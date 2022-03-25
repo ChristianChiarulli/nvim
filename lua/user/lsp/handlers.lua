175ffb7fd6ca8cab54e9a8063b99a194b380a6c7
ChristianChiarulli <chris.machine@pm.me>
2022-03-25 09:40:33 -0400

    only java

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -83,11 +83,11 @@ M.on_attach = function(client, bufnr)
   if client.name == "tsserver" or client.name == "html" then
     client.resolved_capabilities.document_formatting = false
   end
-  vim.lsp.codelens.refresh()
 
   if client.name == "jdt.ls" then
     require("jdtls").setup_dap { hotcodereplace = "auto" }
     require("jdtls.dap").setup_dap_main_class_configs()
+    vim.lsp.codelens.refresh()
   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
