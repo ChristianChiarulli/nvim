5c719ed12a27f5f6c481ef5f52b7301f00e44d7f
ChristianChiarulli <chris.machine@pm.me>
2022-03-25 21:15:12 -0400

    add some augends

--- lua/user/dial.lua
+++ lua/user/dial.lua
@@ -23,10 +23,26 @@ dial_config.augends:register_group {
     augend.constant.alias.Alpha,
   },
   mygroup = {
+    augend.constant.new {
+      elements = { "and", "or" },
+      word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+      cyclic = true, -- "or" is incremented into "and".
+    },
+    augend.constant.new {
+      elements = { "True", "False" },
+      word = true,
+      cyclic = true,
+    },
+    augend.constant.new {
+      elements = { "&&", "||" },
+      word = false,
+      cyclic = true,
+    },
     augend.date.alias["%m/%d/%Y"], -- date (02/19/2022, etc.)
     augend.constant.alias.bool, -- boolean value (true <-> false)
     augend.integer.alias.decimal,
     augend.integer.alias.hex,
+    augend.semver.alias.semver
   },
 }
 
