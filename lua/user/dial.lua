a4c0469b2b1724a39ec850037f0cce5515678aa5
ChristianChiarulli <chris.machine@pm.me>
2022-03-25 21:07:09 -0400

    update dial

--- lua/user/dial.lua
+++ lua/user/dial.lua
@@ -1,26 +1,44 @@
-local status_ok, dial = pcall(require, "dial")
+local status_ok, dial_config = pcall(require, "dial.config")
 if not status_ok then
-	return
+  return
 end
 
-vim.cmd([[
-  nmap <C-a> <Plug>(dial-increment)
-  nmap <C-x> <Plug>(dial-decrement)
-  vmap <C-a> <Plug>(dial-increment)
-  vmap <C-x> <Plug>(dial-decrement)
-  vmap g<C-a> <Plug>(dial-increment-additional)
-  vmap g<C-x> <Plug>(dial-decrement-additional)
-  ]])
+local augend = require "dial.augend"
+dial_config.augends:register_group {
+  default = {
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+    augend.date.alias["%Y/%m/%d"],
+  },
+  typescript = {
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+    augend.constant.new { elements = { "let", "const" } },
+  },
+  visual = {
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+    augend.date.alias["%Y/%m/%d"],
+    augend.constant.alias.alpha,
+    augend.constant.alias.Alpha,
+  },
+  mygroup = {
+    augend.date.alias["%m/%d/%Y"], -- date (02/19/2022, etc.)
+    augend.constant.alias.bool, -- boolean value (true <-> false)
+    augend.integer.alias.decimal,
+    augend.integer.alias.hex,
+  },
+}
 
-dial.augends["custom#boolean"] = dial.common.enum_cyclic({
-	name = "boolean",
-	strlist = { "true", "false" },
-})
-table.insert(dial.config.searchlist.normal, "custom#boolean")
+local map = require "dial.map"
 
--- For Languages which prefer True/False, e.g. python.
-dial.augends["custom#Boolean"] = dial.common.enum_cyclic({
-	name = "Boolean",
-	strlist = { "True", "False" },
-})
-table.insert(dial.config.searchlist.normal, "custom#Boolean")
+-- change augends in VISUAL mode
+vim.api.nvim_set_keymap("n", "<C-a>", map.inc_normal "mygroup", { noremap = true })
+vim.api.nvim_set_keymap("n", "<C-x>", map.dec_normal "mygroup", { noremap = true })
+vim.api.nvim_set_keymap("v", "<C-a>", map.inc_normal "visual", { noremap = true })
+vim.api.nvim_set_keymap("v", "<C-x>", map.dec_normal "visual", { noremap = true })
+
+vim.cmd [[
+  " enable only for specific FileType
+  autocmd FileType typescript,javascript lua vim.api.nvim_buf_set_keymap(0, "n", "<C-a>", require("dial.map").inc_normal("typescript"), {noremap = true})
+]]
