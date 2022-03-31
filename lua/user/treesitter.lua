82f619253c76f91345257add3fd5ad59b27f5918
Christian Chiarulli <chris.machine@pm.me>
2022-03-30 21:29:10 -0400

    use typescript highlighting for motoko

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,6 +3,9 @@ if not status_ok then
 	return
 end
 
+local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
+ft_to_parser.motoko = "typescript"
+
 configs.setup({
 	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
