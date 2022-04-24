cdbe150801b01b9b6a774c30b83dedc7441373d9
Mahyuddin <yudi.al@gmail.com>
2022-04-25 01:25:34 +0700

    Increase Packet Git timeout

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -31,6 +31,10 @@ end
 
 -- Have packer use a popup window
 packer.init {
+  ensure_dependencies   = true,
+  git = {
+    clone_timeout = 300,
+  },
   display = {
     open_fn = function()
       return require("packer.util").float { border = "rounded" }
