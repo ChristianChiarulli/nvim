81e0fa8ba38e76a3bbd7a8c619e7ab1d0eabda35
Mahyuddin <yudi.al@gmail.com>
2022-04-25 01:47:37 +0700

    Changes colorscheme to sonokai

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -109,6 +109,7 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
   use "rose-pine/neovim"
+  use "sainnhe/sonokai"
   -- use "rebelot/kanagawa.nvim"
 
   -- cmp plugins
