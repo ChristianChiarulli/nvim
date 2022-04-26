4b63efd79de828a197ef284a1a3133d733e93a52
ChristianChiarulli <chris.machine@pm.me>
2022-04-26 00:42:38 -0400

    udpate autocommands

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -2,49 +2,63 @@ vim.cmd [[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo,spectre_panel nnoremap <silent> <buffer> q :close<CR> 
-    autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
-    autocmd CmdWinEnter * quit
   augroup end
-
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
-
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd = 
-  augroup end
-
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
-
-  augroup illuminate_augroup
-    autocmd!
-    autocmd VimEnter * hi link illuminatedWord LspReferenceText
-  augroup END
-
- " let ftToEnable = ['java']
-  augroup codelens
-    autocmd!
-    autocmd BufWritePost *.java lua vim.lsp.codelens.refresh() 
-  augroup END
 
   autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 ]]
 
-vim.api.nvim_create_autocmd({"TextYankPost"}, {
-  callback = function() vim.highlight.on_yank({higroup = 'Visual', timeout = 200}) end,  -- Or myvimfun
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.cmd [[
+    setlocal wrap
+    setlocal spell
+    ]]
+  end,
+})
+
+-- TODO why this no work?
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   callback = 
+--     "++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+-- })
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd "tabdo wincmd ="
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+  callback = function()
+    vim.cmd "quit"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  callback = function()
+    vim.cmd "set formatoptions-=cro"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.java" },
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function()
+    vim.cmd "hi link illuminatedWord LspReferenceText"
+  end,
 })
 
 -- autocmd BufLeave * if (!exists('b:caret')) | let b:caret = winsaveview() | endif
