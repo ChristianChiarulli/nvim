fd5c30f98be0da821213cf7974b76de8123a8e43
ChristianChiarulli <chris.machine@pm.me>
2022-05-11 01:46:26 -0400

    new lsp installer api

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,52 +3,95 @@ if not status_ok then
   return
 end
 
--- TODO: add something to installer later
--- require("lspconfig").motoko.setup {}
+local servers = {
+  "cssls",
+  "cssmodules_ls",
+  "emmet_ls",
+  "html",
+  -- "jdtls",
+  "jsonls",
+  "solc",
+  "sumneko_lua",
+  "tflint",
+  "tsserver",
+  "pyright",
+}
 
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-  local opts = {
+local settings = {
+  ensure_installed = servers,
+  -- automatic_installation = false,
+  ui = {
+    icons = {
+      -- server_installed = "◍",
+      -- server_pending = "◍",
+      -- server_uninstalled = "◍",
+      -- server_installed = "✓",
+      -- server_pending = "➜",
+      -- server_uninstalled = "✗",
+    },
+    keymaps = {
+      toggle_server_expand = "<CR>",
+      install_server = "i",
+      update_server = "u",
+      check_server_version = "c",
+      update_all_servers = "U",
+      check_outdated_servers = "C",
+      uninstall_server = "X",
+    },
+  },
+
+  log_level = vim.log.levels.INFO,
+  -- max_concurrent_installers = 4,
+  -- install_root_dir = path.concat { vim.fn.stdpath "data", "lsp_servers" },
+}
+
+lsp_installer.setup(settings)
+
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+  return
+end
+
+local opts = {}
+
+for _, server in pairs(servers) do
+  opts = {
     on_attach = require("user.lsp.handlers").on_attach,
     capabilities = require("user.lsp.handlers").capabilities,
   }
 
-  if server.name == "jsonls" then
+  if server == "jsonls" then
     local jsonls_opts = require "user.lsp.settings.jsonls"
     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
   end
 
-  if server.name == "sumneko_lua" then
+  if server == "sumneko_lua" then
     local sumneko_opts = require "user.lsp.settings.sumneko_lua"
     opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
   end
 
-  if server.name == "pyright" then
+  if server == "pyright" then
     local pyright_opts = require "user.lsp.settings.pyright"
     opts = vim.tbl_deep_extend("force", pyright_opts, opts)
   end
 
-  if server.name == "jdtls" then
-    return
-  end
-
-  if server.name == "solang" then
+  if server == "solang" then
     local solang_opts = require "user.lsp.settings.solang"
     opts = vim.tbl_deep_extend("force", solang_opts, opts)
   end
 
-  if server.name == "solc" then
+  if server == "solc" then
     local solc_opts = require "user.lsp.settings.solc"
     opts = vim.tbl_deep_extend("force", solc_opts, opts)
   end
 
-  if server.name == "emmet_ls" then
+  if server == "emmet_ls" then
     local emmet_ls_opts = require "user.lsp.settings.emmet_ls"
     opts = vim.tbl_deep_extend("force", emmet_ls_opts, opts)
   end
 
-  -- This setup() function is exactly the same as lspconfig's setup function.
-  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  server:setup(opts)
-end)
+  lspconfig[server].setup(opts)
+end
+
+-- TODO: add something to installer later
+-- require("lspconfig").motoko.setup {}
