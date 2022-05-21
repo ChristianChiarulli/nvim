c3c5e673157cfc6f7e10cf9688b534180027f047
ChristianChiarulli <chris.machine@pm.me>
2022-05-21 05:36:52 -0400

    update cybu

--- lua/user/cybu.lua
+++ lua/user/cybu.lua
@@ -3,6 +3,17 @@ if not ok then
   return
 end
 cybu.setup {
+  position = {
+    relative_to = "win", -- win, editor, cursor
+    anchor = "topright", -- topleft, topcenter, topright,
+    -- centerleft, center, centerright,
+    -- bottomleft, bottomcenter, bottomright
+    -- vertical_offset = 10, -- vertical offset from anchor in lines
+    -- horizontal_offset = 0, -- vertical offset from anchor in columns
+    -- max_win_height = 5, -- height of cybu window in lines
+    -- max_win_width = 0.5, -- integer for absolute in columns
+    -- float for relative to win/editor width
+  },
   display_time = 1500, -- time the cybu window is displayed
   style = {
     path = "relative", -- absolute, relative, tail (filename only)
