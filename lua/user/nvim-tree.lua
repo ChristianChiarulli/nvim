a528dea44f9ac91f3167a2dc77113827c40bb332
Christian Chiarulli <chris.machine@pm.me>
2022-05-21 04:37:20 -0400

    udpate filter

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -50,6 +50,10 @@ nvim_tree.setup {
     "dashboard",
     "alpha",
   },
+  filters = {
+    custom = { ".git" },
+    exclude = { ".gitignore" },
+  },
   -- auto_close = true,
   -- open_on_tab = false,
   -- hijack_cursor = false,
