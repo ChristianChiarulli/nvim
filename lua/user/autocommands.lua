0f6903b74f8751b57516135b323d101bb96f56df
ChristianChiarulli <chris.machine@pm.me>
2022-05-23 15:04:48 -0400

    q close lir

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -8,7 +8,7 @@
 -- })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
   callback = function()
     vim.cmd [[
       nnoremap <silent> <buffer> q :close<CR> 
