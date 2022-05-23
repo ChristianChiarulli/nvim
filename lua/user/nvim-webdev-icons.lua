045f2af8bc16ab635a53c34106f2b138a66bada0
ChristianChiarulli <chris.machine@pm.me>
2022-05-23 01:35:54 -0400

    pcall for webdev icons

--- lua/user/nvim-webdev-icons.lua
+++ lua/user/nvim-webdev-icons.lua
@@ -1,4 +1,9 @@
-require("nvim-web-devicons").set_icon {
+local status_ok, nvim_web_devicons = pcall(require, "nvim-web-devicons")
+if not status_ok then
+  return
+end
+
+nvim_web_devicons.set_icon {
   sh = {
     icon = "",
     color = "#1DC123",
