3d0578b002e8460aeed3c11ea27b7cb1cb787086
ChristianChiarulli <chris.machine@pm.me>
2022-05-28 17:52:27 -0400

    don't use snippets built into jdtls because they don't work

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+
 M.setup = function()
   local icons = require "user.icons"
   local signs = {
@@ -47,11 +49,11 @@ end
 
 local function lsp_highlight_document(client)
   -- if client.server_capabilities.document_highlight then
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-      return
-    end
-    illuminate.on_attach(client)
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if not status_ok then
+    return
+  end
+  illuminate.on_attach(client)
   -- end
 end
 
@@ -85,21 +87,21 @@ M.on_attach = function(client, bufnr)
   if client.name == "jdt.ls" then
     require("jdtls").setup_dap { hotcodereplace = "auto" }
     require("jdtls.dap").setup_dap_main_class_configs()
+    M.capabilities.textDocument.completion.completionItem.snippetSupport = false
     vim.lsp.codelens.refresh()
+  else
+    local status_cmp_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+    if not status_cmp_ok then
+      return
+    end
+
+    M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+    M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
   end
+
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
 end
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
-  return
-end
-
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 
 function M.enable_format_on_save()
   vim.cmd [[
