84872fbf5bb57ec89fdce2642aa333527bedda43
ChristianChiarulli <chris.machine@pm.me>
2022-05-28 01:41:14 -0400

    dont use indent lines

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -49,7 +49,7 @@ nvim_tree.setup {
     highlight_opened_files = "none",
     root_folder_modifier = ":~",
     indent_markers = {
-      enable = true,
+      enable = false,
       icons = {
         corner = "└ ",
         edge = "│ ",
