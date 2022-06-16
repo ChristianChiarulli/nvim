f126bb6746317bfc730cdf63e871327ac276177e
Christian Chiarulli <chris.machine@pm.me>
2022-06-16 00:56:41 -0400

    cmd height = 0, too many messages

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,7 @@
 local options = {
   backup = false,                          -- creates a backup file
   clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 0,                           -- more space in the neovim command line for displaying messages
+  cmdheight = 1,                           -- more space in the neovim command line for displaying messages
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0,                        -- so that `` is visible in markdown files
   -- colorcolumn = "80",
