b24a52679600c445daace7c32a4ccebdd17a8039
Christian Chiarulli <chris.machine@pm.me>
2022-06-18 00:42:43 -0400

    remove jabs

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -88,10 +88,6 @@ return packer.startup(function(use)
     run = "cd app && npm install",
     ft = "markdown",
   }
-  use {
-    "christianchiarulli/JABS.nvim",
-    requires = { "kyazdani42/nvim-web-devicons" }, --optional
-  }
 
   use {
     "ghillb/cybu.nvim",
