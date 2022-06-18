d3fd86a5e05f23ad6d50fe4e2d9ac5e90f0099a3
Mahyuddin <yudi.al@gmail.com>
2022-06-19 01:04:36 +0700

    Configure lualine style

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -3,13 +3,13 @@ if not status_ok then
   return
 end
 
-local status_theme_ok, theme = pcall(require, "lualine.themes.darkplus_dark")
+local status_theme_ok, theme = pcall(require, "lualine.themes.darkneo")
 if not status_theme_ok then
   return
 end
 
 vim.api.nvim_set_hl(0, "SLGitIcon", { fg = "#E8AB53", bg = "#303030" })
-vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#D4D4D4", bg = "#303030", bold = false })
+vim.api.nvim_set_hl(0, "SLBranchName", { fg = "#D4D4D4", bg = "#303030", bold = true, italic = true })
 -- vim.api.nvim_set_hl(0, "SLProgress", { fg = "#D7BA7D", bg = "#252525" })
 vim.api.nvim_set_hl(0, "SLProgress", { fg = "#D4D4D4", bg = "#303030" })
 vim.api.nvim_set_hl(0, "SLSeparator", { fg = "#808080", bg = "#252525" })
@@ -38,18 +38,53 @@ local mode_color = {
   t = "#D7BA7D",
 }
 
-local mode = {
-  -- mode component
-  function()
-    return "▊"
-  end,
-  color = function()
-    -- auto change color according to neovims mode
-    return { fg = mode_color[vim.fn.mode()] }
-  end,
-  -- padding = { right = 1 },
-  padding = 0,
-}
+local function getWords()
+  if vim.bo.filetype == "md" or vim.bo.filetype == "txt" or vim.bo.filetype == "markdown" then
+    if vim.fn.wordcount().visual_words == 1 then
+      return tostring(vim.fn.wordcount().visual_words) .. " word"
+    elseif not (vim.fn.wordcount().visual_words == nil) then
+      return tostring(vim.fn.wordcount().visual_words) .. " words"
+    else
+      return tostring(vim.fn.wordcount().words) .. " words"
+    end
+  else
+    return ""
+  end
+end
+
+-- local lineNum = vim.api.nvim_win_get_cursor(0)[1]
+local function getLines()
+  return tostring(vim.api.nvim_win_get_cursor(0)[1]) .. "/" .. tostring(vim.api.nvim_buf_line_count(0))
+end
+
+local function getColumn()
+  local val = vim.api.nvim_win_get_cursor(0)[2]
+  -- pad value to 3 units to stop geometry shift
+  return string.format("%03d", val)
+end
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+-- local mode = {
+--   -- mode component
+--   function()
+--     return "▊"
+--   end,
+--   color = function()
+--     -- auto change color according to neovims mode
+--     return { fg = mode_color[vim.fn.mode()] }
+--   end,
+--   -- padding = { right = 1 },
+--   padding = 0,
+-- }
 
 local hide_in_width = function()
   return vim.fn.winwidth(0) > 80
@@ -62,14 +97,14 @@ local diagnostics = {
   sources = { "nvim_diagnostic" },
   sections = { "error", "warn" },
   symbols = { error = icons.diagnostics.Error .. " ", warn = icons.diagnostics.Warning .. " " },
-  colored = false,
+  colored = true,
   update_in_insert = false,
   always_visible = true,
 }
 
 local diff = {
   "diff",
-  colored = false,
+  colored = true,
   symbols = { added = icons.git.Add .. " ", modified = icons.git.Mod .. " ", removed = icons.git.Remove .. " " }, -- changes diff symbols
   cond = hide_in_width,
   separator = "%#SLSeparator#" .. "│ " .. "%*",
@@ -83,16 +118,14 @@ local diff = {
 -- }
 local mode = {
   "mode",
-  fmt = function(str)
-    return "-- " .. str .. " --"
+  fmt = function(res)
+    return res
   end,
 }
 
 local filetype = {
   "filetype",
   icons_enabled = true,
-  -- icon = nil,
-  icons_enabled = false,
   icon = nil,
 }
 
@@ -101,7 +134,7 @@ local branch = {
   icons_enabled = true,
   icon = "%#SLGitIcon#" .. "" .. "%*" .. "%#SLBranchName#",
   -- color = "Constant",
-  colored = false,
+  colored = true,
 }
 
 local progress = {
@@ -120,21 +153,6 @@ local progress = {
   -- padding = 0,
 }
 
--- local progress = {
---   "progress",
---   fmt = function(str)
---     print(vim.fn.expand(str))
---     if str == "1%" then
---       return "TOP"
---     end
---     if str == "100%" then
---       return "BOT"
---     end
---     return str
---   end,
---   -- padding = 0,
--- }
-
 local current_signature = function()
   if not pcall(require, "lsp_signature") then
     return
@@ -143,17 +161,6 @@ local current_signature = function()
   -- return sig.label .. "🐼" .. sig.hint
   return "%#SLSeparator#" .. sig.hint .. "%*"
 end
-
--- cool function for progress
--- local progress = function()
---   local current_line = vim.fn.line "."
---   local total_lines = vim.fn.line "$"
---   local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
---   local line_ratio = current_line / total_lines
---   local index = math.ceil(line_ratio * #chars)
---   -- return chars[index]
---   return "%#SLProgress#" .. chars[index] .. "%*"
--- end
 
 local spaces = {
   function()
@@ -170,16 +177,6 @@ local location = {
   end,
 }
 
--- cool function for progress
-local progress = function()
-  local current_line = vim.fn.line "."
-  local total_lines = vim.fn.line "$"
-  local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-  local line_ratio = current_line / total_lines
-  local index = math.ceil(line_ratio * #chars)
-  return chars[index]
-end
-
 local spaces = function()
   return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
@@ -191,29 +188,20 @@ lualine.setup {
     -- theme = "auto",
     theme = theme,
     component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = { "alpha", "dashboard" },
     section_separators = { left = "", right = "" },
-    -- disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "toggleterm" },
-    disabled_filetypes = { "alpha", "dashboard", "toggleterm" },
+    disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline", "toggleterm" },
     always_divide_middle = true,
   },
   sections = {
-    lualine_a = { mode, branch },
-    lualine_b = { diagnostics },
-    lualine_c = { { current_signature, cond = hide_in_width } },
-    -- lualine_x = { diff, spaces, "encoding", filetype },
-    lualine_x = { diff, spaces, filetype },
-    lualine_y = { progress },
-    lualine_z = { location },
-    lualine_c = {},
-    -- lualine_c = {
-    --   { nvim_gps, cond = hide_in_width },
-    -- },
-    -- lualine_x = { "encoding", "fileformat", "filetype" },
-    lualine_x = { diff, spaces, "encoding", filetype },
-    lualine_y = { location },
-    lualine_z = { progress },
+    lualine_a = { mode },
+    lualine_b = { branch, diff },
+    lualine_c = { },
+    lualine_x = { },
+    lualine_y = { diagnostics, filetype },
+    lualine_z = {
+      { getColumn, padding = { left = 1, right = 0 } },
+      { getLines, icon = "", padding = 1 },
+    },
   },
   inactive_sections = {
     lualine_a = {},
@@ -224,5 +212,7 @@ lualine.setup {
     lualine_z = {},
   },
   tabline = {},
-  extensions = {},
+  extensions = {
+    "quickfix",
+  },
 }
