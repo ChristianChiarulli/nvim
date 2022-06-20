fd10ab1752beeea703fab1c4e84ae7c7d62f473a
ChristianChiarulli <chris.machine@pm.me>
2022-06-20 11:17:28 -0400

    update hop

--- lua/user/hop.lua
+++ lua/user/hop.lua
@@ -4,4 +4,4 @@ if not status_ok then
 end
 hop.setup()
 vim.api.nvim_set_keymap("", "s", ":HopChar2<cr>", { silent = true })
-vim.api.nvim_set_keymap("", "S", ":HopWord<cr>", { silent = true })
+vim.api.nvim_set_keymap("", "S", ":HopWordCurrentLine<cr>", { silent = true })
