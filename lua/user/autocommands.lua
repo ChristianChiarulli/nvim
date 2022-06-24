850da6bf82a7f56c02ce98f9a2b63451ce4ebdbc
Christian Chiarulli <chris.machine@pm.me>
2022-06-24 00:44:40 -0400

    this always bugged me

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -93,3 +93,12 @@ vim.api.nvim_create_autocmd({ "VimEnter" }, {
     vim.cmd "hi link illuminatedWord LspReferenceText"
   end,
 })
+
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function()
+    local luasnip = require "luasnip"
+    if luasnip.expand_or_jumpable() then
+      luasnip.unlink_current()
+    end
+  end,
+})
