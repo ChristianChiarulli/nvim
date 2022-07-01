40289bc86e2ec79e85af92d71a2b5a1d785ba296
Christian Chiarulli <chris.machine@pm.me>
2022-07-01 16:59:14 -0400

    update indentline

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -20,29 +20,30 @@ vim.g.indent_blankline_char = "▏"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_use_treesitter_scope = 1
 vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-  "class",
-  "return",
-  "function",
-  "method",
-  "^if",
-  "^while",
-  "jsx_element",
-  "^for",
-  "^object",
-  "^table",
-  "block",
-  "arguments",
-  "if_statement",
-  "else_clause",
-  "jsx_element",
-  "jsx_self_closing_element",
-  "try_statement",
-  "catch_clause",
-  "import_statement",
-  "operation_type",
-}
+-- vim.g.indent_blankline_context_patterns = {
+--   "class",
+--   "return",
+--   "function",
+--   "method",
+--   "^if",
+--   "^while",
+--   "jsx_element",
+--   "^for",
+--   "^object",
+--   "^table",
+--   "block",
+--   "arguments",
+--   "if_statement",
+--   "else_clause",
+--   "jsx_element",
+--   "jsx_self_closing_element",
+--   "try_statement",
+--   "catch_clause",
+--   "import_statement",
+--   "operation_type",
+-- }
 -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
 -- vim.wo.colorcolumn = "99999"
 
