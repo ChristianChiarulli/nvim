d91c6273fe525f5b0774428cb06296af9341926a
ChristianChiarulli <chris.machine@pm.me>
2022-07-02 00:13:52 -0400

    finally

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -40,6 +40,7 @@ M.setup = function()
 
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
+    width = 60
   })
 
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
