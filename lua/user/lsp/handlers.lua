4174eb4af967ff499366dc00085b1b3178fe4027
ChristianChiarulli <chris.machine@pm.me>
2022-07-04 12:19:11 -0400

    fix min height

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -48,13 +48,13 @@ M.setup = function()
   vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = "rounded",
     width = 60,
-    height = 30,
+    -- height = 30,
   })
 
   vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
     border = "rounded",
     width = 60,
-    height = 30,
+    -- height = 30,
   })
 end
 
