c0e436f4e637f71ea7703d011e7a2f31162fbd7d
ChristianChiarulli <chris.machine@pm.me>
2022-07-05 10:50:30 -0400

    formatting

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,47 +1,48 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
-local ft_to_parser = require"nvim-treesitter.parsers".filetype_to_parsername
-ft_to_parser.motoko = "typescript"
+-- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+-- ft_to_parser.motoko = "typescript"
 
-configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
+configs.setup {
+  ensure_installed = "all", -- one of "all" or a list of languages
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
     -- use_languagetree = true,
-		enable = true, -- false will disable the whole extension
-		-- disable = { "css", "html" }, -- list of language that will be disabled
-		disable = { "css", "markdown" }, -- list of language that will be disabled
-		-- additional_vim_regex_highlighting = true,
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css", "rust" } },
-	context_commentstring = {
-		enable = true,
-		enable_autocmd = false,
-	},
-	autotag = {
-		enable = true,
-		disable = { "xml", "markdown" },
-	},
-	rainbow = {
-		enable = false,
-		colors = {
-			"Gold",
-			"Orchid",
-			"DodgerBlue",
-			-- "Cornsilk",
-			-- "Salmon",
-			-- "LawnGreen",
-		},
-		disable = { "html" },
-	},
-	playground = {
-		enable = true,
-	},
-})
+    enable = true, -- false will disable the whole extension
+    -- disable = { "css", "html" }, -- list of language that will be disabled
+    -- disable = { "css", "markdown" }, -- list of language that will be disabled
+    disable = { "markdown" }, -- list of language that will be disabled
+    -- additional_vim_regex_highlighting = true,
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css", "rust" } },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  autotag = {
+    enable = true,
+    disable = { "xml", "markdown" },
+  },
+  rainbow = {
+    enable = false,
+    colors = {
+      "Gold",
+      "Orchid",
+      "DodgerBlue",
+      -- "Cornsilk",
+      -- "Salmon",
+      -- "LawnGreen",
+    },
+    disable = { "html" },
+  },
+  playground = {
+    enable = true,
+  },
+}
