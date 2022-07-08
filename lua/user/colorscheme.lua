a41a35ead1946c6a19676657ae8891d0314ec985
ChristianChiarulli <chris.machine@pm.me>
2022-07-08 14:11:25 -0400

    don't notify

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,6 +2,6 @@ local colorscheme = "onedarker"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  -- vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
