3de8fcc901b4739a3ab0b9b5b1a2b52b25deb380
ChristianChiarulli <chris.machine@pm.me>
2022-07-09 21:28:01 -0400

    fix

--- lua/user/auto-session.lua
+++ lua/user/auto-session.lua
@@ -1,3 +1,8 @@
+local status_ok, auto_session = pcall(require, "auto-session")
+if not status_ok then
+  return
+end
+
 local opts = {
   log_level = "info",
   auto_session_enable_last_session = false,
@@ -11,4 +16,4 @@ local opts = {
   bypass_session_save_file_types = { "alpha" },
 }
 
-require("auto-session").setup(opts)
+auto_session.setup(opts)
