41914c74d6696a027463842b05cf7c2aba366036
ChristianChiarulli <chris.machine@pm.me>
2022-07-11 22:21:07 -0400

    whichkey instant

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,6 +10,7 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
 
 -- Modes
 --   normal_mode = "n",
