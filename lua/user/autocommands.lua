d5d9e5b0b32d7733154fe6ae3ec563d31226dec9
ChristianChiarulli <chris.machine@pm.me>
2022-07-12 12:41:20 -0400

    better for Jaq

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -17,10 +17,21 @@ vim.api.nvim_create_autocmd({ "User" }, {
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir", "DressingSelect" },
+  pattern = { "Jaq", "qf", "help", "man", "lspinfo", "spectre_panel", "lir", "DressingSelect" },
   callback = function()
     vim.cmd [[
       nnoremap <silent> <buffer> q :close<CR> 
+      set nobuflisted 
+    ]]
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "Jaq" },
+  callback = function()
+    vim.cmd [[
+      nnoremap <silent> <buffer> <m-r> :close<CR>
+      " nnoremap <silent> <buffer> <m-r> <NOP> 
       set nobuflisted 
     ]]
   end,
