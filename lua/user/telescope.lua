ca05834a4aea5c686d387b5c5b78b0be8f98d3c7
ChristianChiarulli <chris.machine@pm.me>
2022-07-12 11:45:15 -0400

    standard whichkey

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -98,7 +98,7 @@ telescope.setup {
         ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
         ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
         ["<C-l>"] = actions.complete_tag,
-        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+        ["<C-h>"] = actions.which_key, -- keys from pressing <C-h>
         ["<esc>"] = actions.close,
       },
 
