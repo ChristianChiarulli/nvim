9e1f61aa6766a1052d4e6a51e839425cf10e5c12
ChristianChiarulli <chris.machine@pm.me>
2022-07-23 19:56:05 -0400

    normal hl don't work here

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -3,9 +3,16 @@ if not status_ok then
   return
 end
 
+local dashboard = require "alpha.themes.dashboard"
+
+local function button(sc, txt, keybind, keybind_opts)
+  local b = dashboard.button(sc, txt, keybind, keybind_opts)
+  b.opts.hl_shortcut = "Macro"
+  return b
+end
+
 local icons = require "user.icons"
 
-local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = {
   [[                               __                ]],
   [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
@@ -15,20 +22,16 @@ dashboard.section.header.val = {
   [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-  dashboard.button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button(
-    "p",
-    icons.git.Repo .. " Find project",
-    ":lua require('telescope').extensions.projects.projects()<CR>"
-  ),
-  dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-  dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
+  button("f", icons.documents.Files .. " Find file", ":Telescope find_files <CR>"),
+  button("e", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+  button("p", icons.git.Repo .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+  button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
+  button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
   -- dashboard.button("s", icons.ui.SignIn .. " Find Session", ":silent Autosession search <CR>"),
-  dashboard.button("s", icons.ui.SignIn .. " Find Session", ":SearchSession<CR>"),
-  dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-  dashboard.button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
-  dashboard.button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
+  button("s", icons.ui.SignIn .. " Find Session", ":SearchSession<CR>"),
+  button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
+  button("u", icons.ui.CloudDownload .. " Update", ":PackerSync<CR>"),
+  button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
 }
 local function footer()
   -- NOTE: requires the fortune-mod package to work
