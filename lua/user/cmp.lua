ce334233eaf6768e19222dfdb3a35f50814c94b2
ChristianChiarulli <chris.machine@pm.me>
2022-07-24 07:16:48 -0400

    more

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -49,6 +49,7 @@ cmp.setup {
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<m-o>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
     ["<C-c>"] = cmp.mapping {
       i = cmp.mapping.abort(),
@@ -59,6 +60,10 @@ cmp.setup {
       c = cmp.mapping.close(),
     },
     ["<m-k>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
+    ["<m-c>"] = cmp.mapping {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
