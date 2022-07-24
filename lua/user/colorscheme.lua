bcb681f31f30414f2445c104be9cc95d9064b701
ChristianChiarulli <chris.machine@pm.me>
2022-07-24 01:36:30 -0400

    options for onedarker

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,15 @@
 local colorscheme = "onedarker"
 
+vim.g.onedarker_italic_keywords = true
+
+vim.g.onedarker_italic_functions = false
+
+vim.g.onedarker_italic_comments = true
+
+vim.g.onedarker_italic_loops = true
+
+vim.g.onedarker_italic_conditionals = true
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   -- vim.notify("colorscheme " .. colorscheme .. " not found!")
