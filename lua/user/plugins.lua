52de8e6e3155faa7ec3b653b77534c98720bec87
ChristianChiarulli <chris.machine@pm.me>
2022-07-24 21:35:05 -0400

    snapshots

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -2,6 +2,7 @@ local fn = vim.fn
 
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+---@diagnostic disable-next-line: missing-parameter
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system {
     "git",
@@ -31,10 +32,13 @@ end
 
 -- Have packer use a popup window
 packer.init {
+  -- snapshot = "july-24",
+  snapshot_path = fn.stdpath "config" .. "/snapshots",
   display = {
     open_fn = function()
       return require("packer.util").float { border = "rounded" }
     end,
+    prompt_border = "rounded", -- Border style of prompt popups.
   },
 }
 
