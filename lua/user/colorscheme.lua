3661c89de5146ede14810eb4cafed108329f15cf
ChristianChiarulli <chris.machine@pm.me>
2022-07-26 18:10:06 -0400

    don't italic for comic code

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,14 +1,14 @@
 local colorscheme = "onedarker"
 
-vim.g.onedarker_italic_keywords = true
+vim.g.onedarker_italic_keywords = false
 
 vim.g.onedarker_italic_functions = false
 
 vim.g.onedarker_italic_comments = true
 
-vim.g.onedarker_italic_loops = true
+vim.g.onedarker_italic_loops = false
 
-vim.g.onedarker_italic_conditionals = true
+vim.g.onedarker_italic_conditionals = false
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
