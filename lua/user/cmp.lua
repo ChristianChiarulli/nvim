5fe07fe9b811c225109ded93b445a5c1e1f607d7
ChristianChiarulli <chris.machine@pm.me>
2022-07-26 03:57:33 -0400

    can get annoying

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -187,10 +187,11 @@ cmp.setup {
     select = false,
   },
   window = {
-    documentation = {
-      border = "rounded",
-      winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
-    },
+    documentation = false,
+    -- documentation = {
+    --   border = "rounded",
+    --   winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+    -- },
     completion = {
       border = "rounded",
       winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
