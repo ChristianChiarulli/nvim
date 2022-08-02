f9b064c203222508f06c66c976a8312eba2b0ce1
ChristianChiarulli <chris.machine@pm.me>
2022-08-02 19:28:52 -0400

    toggle hints

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -267,7 +267,8 @@ local mappings = {
     f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
     F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
     i = { "<cmd>LspInfo<cr>", "Info" },
-    h = { "<cmd>IlluminationToggle<cr>", "Toggle Doc HL" },
+    h = { "<cmd>require('lsp-inlayhints').toggle()<cr>", "Toggle Hints" },
+    H = { "<cmd>IlluminationToggle<cr>", "Toggle Doc HL" },
     I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
     j = {
       "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
