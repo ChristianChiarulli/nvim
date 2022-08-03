5b240a9a274927727c19b12c71be7fe57ebf0231
ChristianChiarulli <chris.machine@pm.me>
2022-08-03 14:15:38 -0400

    updates

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -90,7 +90,8 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-emoji"
   use "hrsh7th/cmp-nvim-lua"
   use "zbirenbaum/copilot-cmp"
-  use { "tzachar/cmp-tabnine", run = "./install.sh" }
+  use { "tzachar/cmp-tabnine", commit = "1a8fd2795e4317fd564da269cc64a2fa17ee854e", 
+run = "./install.sh" }
 
   -- Snippet
   use "L3MON4D3/LuaSnip" --snippet engine
