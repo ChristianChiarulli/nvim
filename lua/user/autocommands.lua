5639c280dbda71c0f858821f7f47ccde0e158468
ChristianChiarulli <chris.machine@pm.me>
2022-08-03 21:20:52 -0400

    fixes

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -172,7 +172,10 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
 
 vim.api.nvim_create_autocmd({ "CursorHold" }, {
   callback = function()
-    local luasnip = require "luasnip"
+    local status_ok, luasnip = pcall(require, "luasnip")
+    if not status_ok then
+      return
+    end
     if luasnip.expand_or_jumpable() then
       -- ask maintainer for option to make this silent
       -- luasnip.unlink_current()
--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -18,6 +18,7 @@ vim.g.indentLine_enabled = 1
 -- vim.g.indent_blankline_char = "│"
 vim.g.indent_blankline_char = "▏"
 -- vim.g.indent_blankline_char = "▎"
+
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 vim.g.indent_blankline_show_first_indent_level = true
 vim.g.indent_blankline_use_treesitter = false
