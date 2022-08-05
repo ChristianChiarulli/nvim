f326c071fee497ed1aef8f99f86b62cfa8a4c150
Anant Murmu <anant.murmu@yahoo.com>
2022-08-05 12:33:33 +0530

    fix: install script * executing fail-safe cd command * more relaxed shebang for bash

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/env bash
 
 git clone https://github.com/neovim/neovim.git ~/.config/nvim/neovim
 
 sudo rm /usr/local/bin/nvim
 sudo rm -r /usr/local/share/nvim/
 
-cd ~/.config/nvim/neovim
+cd ~/.config/nvim/neovim || echo "Error: neovim directory not found exiting script ..." && exit
 
 make CMAKE_BUILD_TYPE=Release
 
