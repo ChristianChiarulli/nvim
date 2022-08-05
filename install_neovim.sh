d4c12faae62ea17b6349dc3f5d32fcecf37cc917
Anant Murmu <freezboltz@gmail.com>
2022-08-06 03:14:35 +0530

    fix: install script (#49)

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,14 +1,45 @@
-#!/bin/bash
+#!/bin/env bash
 
-git clone https://github.com/neovim/neovim.git ~/.config/nvim/neovim
+#function for dependency check
+dep_ch() {
+	for dep; do
+		if ! command -v "$dep" >/dev/null; then
+			terminate_script "Program \"$dep\" not found. Please install it."
+		fi
+	done
+}
 
-sudo rm /usr/local/bin/nvim
-sudo rm -r /usr/local/share/nvim/
+# display error message in red to stderr
+error() {
+	print "\33[2k\r\033[1;31m%s\033[0m\n" "$*" >&2
+}
 
-cd ~/.config/nvim/neovim
+#function to terminate script if error occur
+terminate_script() {
+	error "$*"
+	exit 1
+}
+
+#provide dependency (space seperated)
+dep_ch gcc cmake
+
+CURRENT_DIR=$(pwd)
+NEOVIM_DIR="$HOME/.config/nvim/neovim"
+
+#checks if neovim directory already exists
+[ ! -d "$NEOVIM_DIR" ] || rm -rf "$NEOVIM_DIR"
+
+#cloning latest neovim from official repo
+git clone https://github.com/neovim/neovim.git "$NEOVIM_DIR"
+
+#checks for file existance
+[ -e /usr/local/bin/nvim ] && sudo rm -f /usr/local/bin/nvim
+[ -e /usr/local/share/nvim/ ] && sudo rm -rf /usr/local/share/nvim/
+
+cd "$NEOVIM_DIR" 2>/dev/null || terminate_script "Error: neovim directory not found exiting script ..."
 
 make CMAKE_BUILD_TYPE=Release
 
 sudo make install
-
-rm -rf ~/.config/nvim/neovim
+cd "$CURRENT_DIR" || terminate_script "Error: not able to change directory to \"$CURRENT_DIR\""
+rm -rf "$NEOVIM_DIR"
