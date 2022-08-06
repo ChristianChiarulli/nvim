9f34af1e076d5c686f6100fd724938549fb40942
ChristianChiarulli <chris.machine@pm.me>
2022-08-06 02:11:56 -0400

    update

--- lua/user/surround.lua
+++ lua/user/surround.lua
@@ -16,55 +16,56 @@ surround.setup {
     delete = "ds",
     change = "cs",
   },
-  delimiters = {
-    pairs = {
-      ["("] = { "( ", " )" },
-      [")"] = { "(", ")" },
-      ["{"] = { "{ ", " }" },
-      ["}"] = { "{", "}" },
-      ["<"] = { "< ", " >" },
-      [">"] = { "<", ">" },
-      ["["] = { "[ ", " ]" },
-      ["]"] = { "[", "]" },
-      -- Define pairs based on function evaluations!
-      ["i"] = function()
-        return {
-          require("nvim-surround.utils").get_input "Enter the left delimiter: ",
-          require("nvim-surround.utils").get_input "Enter the right delimiter: ",
-        }
-      end,
-      ["f"] = function()
-        return {
-          require("nvim-surround.utils").get_input "Enter the function name: " .. "(",
-          ")",
-        }
-      end,
-    },
-    separators = {
-      ["'"] = { "'", "'" },
-      ['"'] = { '"', '"' },
-      ["`"] = { "`", "`" },
-    },
-    HTML = {
-      ["t"] = "type", -- Change just the tag type
-      ["T"] = "whole", -- Change the whole tag contents
-    },
-    aliases = {
-      ["a"] = ">", -- Single character aliases apply everywhere
-      ["p"] = ")",
-      ["b"] = "]",
-      ["r"] = "}",
-      -- Table aliases only apply for changes/deletions
-      ["q"] = { '"', "'", "`" }, -- Any quote character
-      ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
-    },
-    invalid_key_behavior = function()
-      vim.notify ("Invalid surround character", vim.log.levels.INFO)
-    end,
-  },
-  highlight_motion = { -- Highlight before inserting/changing surrounds
-    duration = 0,
-  },
+  -- TODO: come back and fix this
+  -- surrounds = {
+  --   pairs = {
+  --     ["("] = { "( ", " )" },
+  --     [")"] = { "(", ")" },
+  --     ["{"] = { "{ ", " }" },
+  --     ["}"] = { "{", "}" },
+  --     ["<"] = { "< ", " >" },
+  --     [">"] = { "<", ">" },
+  --     ["["] = { "[ ", " ]" },
+  --     ["]"] = { "[", "]" },
+  --     -- Define pairs based on function evaluations!
+  --     ["i"] = function()
+  --       return {
+  --         require("nvim-surround.utils").get_input "Enter the left delimiter: ",
+  --         require("nvim-surround.utils").get_input "Enter the right delimiter: ",
+  --       }
+  --     end,
+  --     ["f"] = function()
+  --       return {
+  --         require("nvim-surround.utils").get_input "Enter the function name: " .. "(",
+  --         ")",
+  --       }
+  --     end,
+  --   },
+  --   separators = {
+  --     ["'"] = { "'", "'" },
+  --     ['"'] = { '"', '"' },
+  --     ["`"] = { "`", "`" },
+  --   },
+  --   HTML = {
+  --     ["t"] = "type", -- Change just the tag type
+  --     ["T"] = "whole", -- Change the whole tag contents
+  --   },
+  --   aliases = {
+  --     ["a"] = ">", -- Single character aliases apply everywhere
+  --     ["p"] = ")",
+  --     ["b"] = "]",
+  --     ["r"] = "}",
+  --     -- Table aliases only apply for changes/deletions
+  --     ["q"] = { '"', "'", "`" }, -- Any quote character
+  --     ["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
+  --   },
+  --   invalid_key_behavior = function()
+  --     vim.notify ("Invalid surround character", vim.log.levels.INFO)
+  --   end,
+  -- },
+  -- highlight = { -- Highlight before inserting/changing surrounds
+  --   duration = 0,
+  -- },
 }
 
 -- local opts = { noremap = true, silent = true }
