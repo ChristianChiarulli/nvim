a6baa3e12980e0d50a08cc55c857f8fab72b3bd1
ChristianChiarulli <chris.machine@pm.me>
2022-08-06 17:02:27 -0400

    alt tab

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -27,6 +27,7 @@ keymap("n", "<m-h>", "<C-w>h", opts)
 keymap("n", "<m-j>", "<C-w>j", opts)
 keymap("n", "<m-k>", "<C-w>k", opts)
 keymap("n", "<m-l>", "<C-w>l", opts)
+keymap("n", "<m-tab>", "<c-6>", opts)
 
 -- Tabs --
 keymap("n", "<m-t>", ":tabnew %<cr>", opts)
