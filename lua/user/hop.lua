d6b748ab88b32afa94f53cbacb5dd4000597515a
ChristianChiarulli <chris.machine@pm.me>
2022-08-05 20:52:53 -0400

    remapping since I never use

--- lua/user/hop.lua
+++ lua/user/hop.lua
@@ -9,10 +9,10 @@ local keymap = vim.api.nvim_set_keymap
 
 
 
-keymap("", "Q", ":HopWordCurrentLine<cr>", { silent = true })
+keymap("", "L", ":HopWordCurrentLine<cr>", { silent = true })
 -- keymap("", "S", ":HopChar2<cr>", { silent = true })
 -- keymap("", "Q", ":HopPattern<cr>", { silent = true })
-keymap("", "q", ":HopChar2<cr>", { silent = true })
+keymap("", "H", ":HopChar2<cr>", { silent = true })
 
 keymap("o", "f", ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>", opts)
 keymap("o", "F", ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>", opts)
