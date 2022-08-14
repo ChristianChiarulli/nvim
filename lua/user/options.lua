98baf60a0c1765b6f0f51315703b2d285cf554ec
iamverysimp1e <ngyntuenhan@protonmail.com>
2022-08-14 22:39:06 +0700

    Fixed disable automatic comment insertion

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -59,7 +59,9 @@ end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+-- vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+vim.cmd [[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]] -- this seem to work 
+
 
 vim.filetype.add {
   extension = {
