d14d15a7db8bc8ee7ba0e93229562ea19f8e5d25
ChristianChiarulli <chris.machine@pm.me>
2022-08-19 20:59:32 -0400

    up to date

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -73,15 +73,6 @@ M.setup = function()
   })
 end
 
-local function lsp_highlight_document(client)
-  -- if client.server_capabilities.document_highlight then
-  local status_ok, illuminate = pcall(require, "illuminate")
-  if not status_ok then
-    return
-  end
-  illuminate.on_attach(client)
-  -- end
-end
 
 local function attach_navic(client, bufnr)
   vim.g.navic_silence = true
@@ -115,7 +106,6 @@ end
 
 M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
-  lsp_highlight_document(client)
   attach_navic(client, bufnr)
 
   if client.name == "tsserver" then
