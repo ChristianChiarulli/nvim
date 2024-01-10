3d855a50c09eba1caaea971349baa13a8e6acbf6
Christian Chiarulli <chris.machine@pm.me>
2024-01-10 16:25:57 -0500

    update icons

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -39,14 +39,14 @@ function M.config()
   }
 
   dashboard.section.buttons.val = {
-    button("f", icons.ui.Files .. " Find file", ":Telescope find_files <CR>"),
-    button("n", icons.ui.NewFile .. " New file", ":ene <BAR> startinsert <CR>"),
+    button("f", icons.ui.Files .. "  Find file", ":Telescope find_files <CR>"),
+    button("n", icons.ui.NewFile .. "  New file", ":ene <BAR> startinsert <CR>"),
     -- button("s", icons.ui.SignIn .. " Load session", ":lua require('persistence').load()<CR>"),
-    button("p", icons.git.Repo .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-    button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
-    button("t", icons.ui.Text .. " Find text", ":Telescope live_grep <CR>"),
-    button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
-    button("q", icons.ui.SignOut .. " Quit", ":qa<CR>"),
+    button("p", icons.git.Repo .. "  Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+    button("r", icons.ui.History .. "  Recent files", ":Telescope oldfiles <CR>"),
+    button("t", icons.ui.Text .. "  Find text", ":Telescope live_grep <CR>"),
+    button("c", icons.ui.Gear .. "  Config", ":e ~/.config/nvim/init.lua <CR>"),
+    button("q", icons.ui.SignOut .. "  Quit", ":qa<CR>"),
   }
   local function footer()
     return "chrisatmachine.com"
