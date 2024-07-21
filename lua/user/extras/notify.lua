0fea66838207293cfccadeffd52cefb88b586fbf
Christian Chiarulli <chris.machine@pm.me>
2024-07-21 13:50:52 -0400

    ignore message

--- lua/user/extras/notify.lua
+++ lua/user/extras/notify.lua
@@ -3,7 +3,15 @@ local M = {
 }
 
 function M.config()
-  vim.notify = require "notify"
+  local banned_messages = { "No information available" }
+  vim.notify = function(msg, ...)
+    for _, banned in ipairs(banned_messages) do
+      if msg == banned then
+        return
+      end
+    end
+    return require "notify"(msg, ...)
+  end
 end
 
 return M
